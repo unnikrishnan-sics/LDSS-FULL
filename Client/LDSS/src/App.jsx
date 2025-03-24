300a066d7d8178288fbdf607b9307edf0ed19474
ArunBenze-4 <arunbenze.sics@gmail.com>
2025-03-24 16:53:41 +0530

    initial commit

---
+++ Client/LDSS/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const arr=[1,2,3,4,5]
+  console.log(arr);
+  console.log("benz");
+  // const str=new String("hello")
+  console.log(String);
+  console.log(typeof(NaN));
+  
+
+  return (
+    <>
+      <h1>LDSS</h1>
+    </>
+  )
+}
+
+export default App
